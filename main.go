3a95e7dd9e4867ccaee3434de8c89a8d55de7ccd
adithyamaheshb <adithyamaheshbariki@gmail.com>
2020-06-07 20:55:30 -0400

    Remove comments about variable declaration

--- main.go
+++ main.go
@@ -3,14 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-
-	// In this type of declaration we are telling the Go
-	// compiler to figure out the type of the variable.
-	// Also we use this type of declaration(:=) only if we
-	// are decalring a new variable otherwise just use ("=")
 	card := "Ace of Spades"
-	card = "Five of Diamonds"
 
 	fmt.Println(card)
 }
