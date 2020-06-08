7aef921881b197cea3ac2f829033739504ffe079
adithyamaheshb <adithyamaheshbariki@gmail.com>
2020-06-07 20:49:39 -0400

    Get started with variables

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi there!")
+	// var card string = "Ace of Spades"
+
+	// In this type of declaration we are telling the Go
+	// compiler to figure out the type of the variable.
+	// Also we use this type of declaration(:=) only if we
+	// are decalring a new variable otherwise just use ("=")
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
